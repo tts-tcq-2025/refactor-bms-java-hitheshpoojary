bf06f82411580794f6328301bf192228f8365489
hitheshpoojary <67824540+hitheshpoojary@users.noreply.github.com>
2025-08-06 20:39:45 +0530

    Update VitalsChecker.java

--- src/main/java/vitals/VitalsChecker.java
+++ src/main/java/vitals/VitalsChecker.java
@@ -4,35 +4,23 @@ import vitals.Compute;
 public abstract class VitalsChecker {
   static boolean vitalsOk(float temperature, float pulseRate, float spo2) 
       throws InterruptedException {
-    if (temperature > 102 || temperature < 95) {
-      System.out.println("Temperature is critical!");
-      for (int i = 0; i < 6; i++) {
-        System.out.print("\r* ");
-        Thread.sleep(1000);
-        System.out.print("\r *");
-        Thread.sleep(1000);
-      }
-      return false;
-    } else if (pulseRate < 60 || pulseRate > 100) {
-      System.out.println("Pulse Rate is out of range!");
-      for (int i = 0; i < 6; i++) {
-        System.out.print("\r* ");
-        Thread.sleep(1000);
-        System.out.print("\r *");
-        Thread.sleep(1000);
-      }
-      return false;
-    } else if (spo2 < 90) {
-      System.out.println("Oxygen Saturation out of range!");
-      for (int i = 0; i < 6; i++) {
-        System.out.print("\r* ");
-        Thread.sleep(1000);
-        System.out.print("\r *");
-        Thread.sleep(1000);
-      }
-      return false;
-    }
-    return true;
+      if (Compute.checkTemperature(temperature)) {
+            alert("Temperature is critical!");
+            return false;
+        }
+
+      if (Compute.checkPulseRate(pulseRate)) {
+            alert("Pulse Rate is out of range!");
+            return false;
+        }
+
+      if (Compute.checkSpo2(spo2)) {
+            alert("Oxygen Saturation is out of range!");
+            return false;
+        }
+
+        return true;
+
   }
   
 private static void alert(String message)throws InterruptedException {
