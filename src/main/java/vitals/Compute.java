39b4809ed3e5e2304ad0b43d0d18ab80530177ec
hitheshpoojary <67824540+hitheshpoojary@users.noreply.github.com>
2025-08-06 20:35:40 +0530

    Update Compute.java

--- src/main/java/vitals/Compute.java
+++ src/main/java/vitals/Compute.java
@@ -1,21 +1,28 @@
 package vitals;
 
-public class Compute{
+public final class Compute {
+
+    private Compute() {}
+
     static final float MAX_PULSE_RATE = 100;
     static final float MIN_PULSE_RATE = 60;
     static final float MIN_SPO2 = 90;
     static final float MAX_TEMP = 102;
     static final float MIN_TEMP = 95;
 
+    private static boolean isOutOfRange(float value, float min, float max) {
+        return value < min || value > max;
+    }
+    
    public static boolean checkTemperature(float temperature) {
-        return temperature > MAX_TEMP || temperature < MIN_TEMP;
+        return isOutOfRange(temperature,MIN_TEMP,MAX_TEMP) ;
     }
 
     public static boolean checkPulseRate(float pulseRate) {
-        return pulseRate < MIN_PULSE_RATE || pulseRate > MAX_PULSE_RATE;
+        return isOutOfRange(pulseRate,MIN_PULSE_RATE,MIN_PULSE_RATE) ;
     }
 
     public static boolean checkSpo2(float spo2) {
-        return spo2 < MIN_SPO2;
+        return  spo2<MIN_SPO2;
     }
 }
