d9b1c21633bb8fd9153ab3c238c3691dc7a91050
hitheshpoojary <67824540+hitheshpoojary@users.noreply.github.com>
2025-08-06 20:29:46 +0530

    Update Compute.java

--- src/main/java/vitals/Compute.java
+++ src/main/java/vitals/Compute.java
@@ -6,9 +6,16 @@ public class Compute{
     static final float MIN_SPO2 = 90;
     static final float MAX_TEMP = 102;
     static final float MIN_TEMP = 95;
-  
 
-    
+   public static boolean checkTemperature(float temperature) {
+        return temperature > MAX_TEMP || temperature < MIN_TEMP;
+    }
 
-  
+    public static boolean checkPulseRate(float pulseRate) {
+        return pulseRate < MIN_PULSE_RATE || pulseRate > MAX_PULSE_RATE;
+    }
+
+    public static boolean checkSpo2(float spo2) {
+        return spo2 < MIN_SPO2;
+    }
 }
