ba0ece673f02d9cd848fdbfc33ea9ef1876d73e9
hitheshpoojary <67824540+hitheshpoojary@users.noreply.github.com>
2025-08-06 20:10:07 +0530

    Update Compute.java

--- src/main/java/vitals/Compute.java
+++ src/main/java/vitals/Compute.java
@@ -1,5 +1,14 @@
 package vitals;
 
 public class Compute{
+    static final float MAX_PULSE_RATE = 100;
+    static final float MIN_PULSE_RATE = 60;
+    static final float MIN_SPO2 = 90;
+    static final float MAX_TEMP = 102;
+    static final float MIN_TEMP = 95;
+  
 
+    
+
+  
 }
