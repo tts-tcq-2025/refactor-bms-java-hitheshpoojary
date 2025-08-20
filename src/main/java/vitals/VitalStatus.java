14cc96a288c5e43b88d5e4658148ece0e208dbe6
hitheshpoojary <67824540+hitheshpoojary@users.noreply.github.com>
2025-08-21 00:46:38 +0530

    Update VitalStatus.java

--- src/main/java/vitals/VitalStatus.java
+++ src/main/java/vitals/VitalStatus.java
@@ -1,5 +1,9 @@
 package vitals;
 
 public enum VitalStatus {
-    CRITICAL_LOW, WARNING_LOW, NORMAL, WARNING_HIGH, CRITICAL_HIGH
+    CRITICAL_LOW,
+    WARNING_LOW,
+    NORMAL,
+    WARNING_HIGH, 
+    CRITICAL_HIGH
 }
